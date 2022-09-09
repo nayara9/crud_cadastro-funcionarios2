14998500004d52acac4aaa9e44c2d62f08d5eff0
unknown <nayara-eng@hotmail.com>
2022-09-09 10:33:48 -0300

    botao procurar

---
+++ gerenciar/lista.js
@@ -0,0 +1,16 @@
+const nomeClientes = [
+    {
+        id:1,
+        nome: nayara,
+    },
+
+    {
+        id:2,
+        nome: snow,
+    },
+
+    {
+        id:3,
+        nome: oi,
+    },
+]
---
+++ gerenciar/scrip.js
@@ -0,0 +1,9 @@
+const btnSeach = document.querySelector("form")
+
+function renderNomes() {
+    for(const nome of nomes) {
+        const option= document.createElement("option")
+    }
+}
+
+
