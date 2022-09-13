67e37695be3ed90915ee6d96457e2175b381da2f
unknown <nayara-eng@hotmail.com>
2022-09-13 06:14:50 -0300

    css

--- gerenciar/lista.mjs
+++ gerenciar/lista.mjs
@@ -1,17 +1,23 @@
 const funcionario = [
     {
         id:1,
+        codigo:50,
         name:"nayara",
+        status: 0,
     },
 
     {
         id:2,
+        codigo:84,
         name: "snow",
+        status:1,
     },
 
     {
         id:3,
+        codigo:87,
         name: "oi",
+        status:1,
     },
 ]
 
--- gerenciar/script.mjs
+++ gerenciar/script.mjs
@@ -1,26 +1,60 @@
 import {funcionario} from "./lista.mjs"
 
-async function init() {
-    renderData()
+
+var cod = document.getElementById("codigo")
+var div = document.getElementById("name")
+var st = document.getElementById("status")
+var texto = '';
+const bdelete = document.getElementById("bdelete");
+ 
+
+function init() {
+escreveName();
+escreveCodigo();
+escreveStatus();
+
+bdelete.addEventListener("click", onDelete);
 }
+
 init()
 
-function randerData () {
-var div = document.getElementById("name");
-var texto = '';
 
-funcionario.forEach(function(item) {
-   
-    texto +=  item.name + "<br>"
-})
 
-div.innerHTML = texto
+function escreveCodigo() {
+    texto = '';
+    funcionario.forEach(function(item) {
+        texto +=  item.codigo + "<br>"
+    })
+    cod.innerHTML = texto;
+    selectItens();
+}
+
+function escreveName() {
+    funcionario.forEach(function(item) {
+        texto +=  item.name + "<br>"
+    })
+    div.innerHTML = texto;
+    selectItens();
+    }
+    
+
+function escreveStatus() {
+    texto = '';
+    funcionario.forEach(function(item) {
+        texto +=  item.status + "<br>"
+    })
+    st.innerHTML = texto;
+    selectItens();
+}
+
+function selectItens() {
 
 }
 
 
 
+function onDelete () {
 
-
-
+    
+}
 
